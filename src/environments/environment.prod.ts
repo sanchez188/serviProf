55155d4a253ca6e48607fdacadae589cfd66cc2c
sanchez188 <35212173+sanchez188@users.noreply.github.com>
2025-09-10 18:19:40 -0600

    Actualizar credenciales de Supabase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'https://xahcororbsrntzcqyrvi.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhhaGNvcm9yYnNybnR6Y3F5cnZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1MjIzNTcsImV4cCI6MjA3MzA5ODM1N30.eKQta47-dKIHEeHZJ985O6g2dbKg_r7gO1QSlGOQYis'
+  supabaseUrl: 'https://iefgwcchydktpnozuhdk.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllZmd3Y2NoeWRrdHBub3p1aGRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1NDgxNjgsImV4cCI6MjA3MzEyNDE2OH0.6wwoia77zgxlli5Wr-QI009hQbvdbKR1CgeHyUYC_b4'
 };
